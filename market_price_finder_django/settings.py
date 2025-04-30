f86fb7d30b3c53357b49938b859c5379ee2a485f
berkan-ridvan <berkanrd@gmail.com>
2025-04-30 06:19:54 -0500

    version04.30.619

--- market_price_finder_django/settings.py
+++ market_price_finder_django/settings.py
@@ -124,9 +124,9 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+# settings.py
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), os.path.join(BASE_DIR, 'media')]
+
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
