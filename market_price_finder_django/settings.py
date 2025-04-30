a1be38232cfb4bbae9c09704c14febfeeec0a728
berkan-ridvan <69767710+berkan-ridvan@users.noreply.github.com>
2025-04-30 05:09:53 -0500

    Update settings.py

--- market_price_finder_django/settings.py
+++ market_price_finder_django/settings.py
@@ -15,6 +15,7 @@ import os
 from decouple import config
 import dj_database_url
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -79,10 +80,7 @@ WSGI_APPLICATION = 'market_price_finder_django.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
 
