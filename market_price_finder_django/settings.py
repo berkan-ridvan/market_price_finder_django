516843bf316a8d8487458702736dbdabf0cdfb0c
berkan-ridvan <berkanrd@gmail.com>
2025-04-30 05:36:07 -0500

    version4.30.0535

--- market_price_finder_django/settings.py
+++ market_price_finder_django/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'market-price-finder-django.onrender.com', '.onrender.com']
 
