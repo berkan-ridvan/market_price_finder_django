887f7e917e5224f63d7baf40bb754501c2f091b2
berkan-ridvan <69767710+berkan-ridvan@users.noreply.github.com>
2025-04-30 05:12:22 -0500

    Update settings.py

--- market_price_finder_django/settings.py
+++ market_price_finder_django/settings.py
@@ -80,7 +80,11 @@ WSGI_APPLICATION = 'market_price_finder_django.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(conn_max_age=600, ssl_require=True)
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # fallback for local dev
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
@@ -120,9 +124,8 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Media files
