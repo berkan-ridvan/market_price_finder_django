f292f869274c60312e241ed10bc4cacec6b302b0
berkan-ridvan <berkanrd@gmail.com>
2025-04-30 06:02:07 -0500

    version4.30.0556

--- market/apps.py
+++ market/apps.py
@@ -4,3 +4,24 @@ from django.apps import AppConfig
 class MarketConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'market'
+
+
+class MarketConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'market'
+
+    def ready(self):
+        from django.contrib.auth import get_user_model
+        from django.db.models.signals import post_migrate
+
+        def create_admin(sender, **kwargs):
+            User = get_user_model()
+            if not User.objects.filter(username='admin').exists():
+                User.objects.create_superuser(
+                    username='admin',
+                    email='admin@example.com',
+                    password='admin12345'
+                )
+                print("✅ Admin user created")
+
+        post_migrate.connect(create_admin, sender=self)
