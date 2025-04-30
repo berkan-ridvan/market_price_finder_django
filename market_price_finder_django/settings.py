f507527cf0f029e569fa6db57d3a2f40e4403aba
berkan-ridvan <69767710+berkan-ridvan@users.noreply.github.com>
2025-04-30 05:15:38 -0500

    Update settings.py

--- market_price_finder_django/settings.py
+++ market_price_finder_django/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
     'default': dj_database_url.config(
         default='sqlite:///db.sqlite3',  # fallback for local dev
         conn_max_age=600,
-        ssl_require=True
+        ssl_require=False
     )
 }
 
