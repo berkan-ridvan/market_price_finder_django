d7b2e25bded80c1119e07427265631db3936bb58
berkan-ridvan <69767710+berkan-ridvan@users.noreply.github.com>
2025-04-30 04:51:57 -0500

    Update settings.py

--- market_price_finder_django/settings.py
+++ market_price_finder_django/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-($m(7^4#ys=7=%(7=)_i9egs%%ibc#)t@_o+h)5)9^yu_-m^!u'
+SECRET_KEY = config('SECRET_KEY', default='your-secret-key-here')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
